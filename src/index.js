abf61af746d2cb69cf40ffed2986d360ed2889d2
Adam Romański <adam.romanski@netguru.com>
2020-05-22 16:22:22 +0200

    Working prototype

--- src/index.js
+++ src/index.js
@@ -1,56 +1,140 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
-let scene, camera, renderer;
-
-scene = new THREE.Scene();
-camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.0001, 10000);
-camera.position.set(0, 0, 50);
-renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setClearColor(0xeeeeee);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.01, 1000);
+const scene = new THREE.Scene();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const handleResize = () => {
-  const { innerWidth, innerHeight } = window;
-  renderer.setSize(innerWidth, innerHeight);
-  camera.aspect = innerWidth / innerHeight;
-  camera.updateProjectionMatrix();
+// ORBIT CONTROLS
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.set(4, 5, 12);
+camera.rotation.set(0, -5, 0);
+controls.update();
+
+// LIGHTS
+scene.add(new THREE.AmbientLight(0xffffff, 1));
+const spotLight = new THREE.SpotLight(0xffffff, 0.75);
+spotLight.position.set(500, 400, 200);
+spotLight.angle = 0.4;
+spotLight.penumbra = 0.05;
+spotLight.decay = 1;
+spotLight.distance = 2000;
+spotLight.castShadow = true;
+scene.add(spotLight);
+spotLight.target.position.set(3, 0, -3);
+scene.add(spotLight.target);
+
+
+const meshFloor = new THREE.Mesh(
+  new THREE.PlaneGeometry(100, 100, 100, 100),
+  new THREE.MeshLambertMaterial({ color: 0xbbbbbb })
+);
+
+meshFloor.rotation.x = -Math.PI / 2;
+meshFloor.position.y = -1.05;
+meshFloor.receiveShadow = true;
+scene.add(meshFloor);
+
+const box = new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), new THREE.MeshLambertMaterial({ color: 0xaaaaaa }));
+box.castShadow = true;
+box.geometry.elementsNeedUpdate = true;
+box.geometry.colorsNeedUpdate = true;
+box.geometry.groupsNeedUpdate = true;
+scene.add(box);
+
+const createBox = () => {
+  return new THREE.Mesh(new THREE.BoxGeometry(2, 2, 2), new THREE.MeshLambertMaterial({ color: 0xaaaaaa }));
 };
 
-const createBox = (s = 2, color = 0xffffff) => {
-  const boxGeo = new THREE.BoxGeometry(s, s, s);
-  const boxMat = new THREE.MeshPhongMaterial({
-    color: 0xfdd927,
-    shininess: 0,
-  });
-  return new THREE.Mesh(boxGeo, boxMat);
-};
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
 
-const createPointLight = (i = 1, color = 0xffffff) => {
-  return new THREE.PointLight(color, i);
-};
+window.addEventListener('mousemove', (e) => {
+  mouse.x = 2 * (e.clientX / window.innerWidth) - 1;
+  mouse.y = 1 - 2 * (e.clientY / window.innerHeight);
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(scene.children);
+  if (intersects.length) {
+    const [currentObj] = intersects;
+    if (currentObj.object.geometry.type === 'BoxGeometry') {
 
-const l1 = createPointLight(1);
-l1.position.set(0, 0, 100);
-scene.add(l1);
-
-let boxes = [];
-
-for (let i = 0; i <= 10; i++) {
-  for (let j = 0; j <= 15; j++) {
-    const newBox = createBox(5);
-    newBox.position.set(6 * j - 35, 7.2 * i-25, 0);
-    boxes.push(newBox);
+      const index = Math.floor(intersects[0].faceIndex / 2);
+      currentObj.object.geometry.faces[index].color.set(0x1122ff);
+    }
   }
-}
+}, false);
 
-boxes.forEach(box => scene.add(box));
+window.addEventListener('mousedown', (e) => {
+  mouse.x = 2 * (e.clientX / window.innerWidth) - 1;
+  mouse.y = 1 - 2 * (e.clientY / window.innerHeight);
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(scene.children);
+  if (intersects.length) {
+    const [currentObj] = intersects;
+    if (currentObj.object.geometry.type === 'BoxGeometry') {
+      console.log(e.button);
+      if (e.button === 2) {
+        const boxToRemove = scene.getObjectByName(currentObj.object.name);
+        scene.remove(boxToRemove);
+      } else {
+        console.log('cycki');
+        const index = Math.floor(intersects[0].faceIndex / 2);
+        const { object: { position: { x, y, z } } } = currentObj;
+        let newBox;
+        switch (index) {
+          case 0:
+            newBox = createBox();
+            newBox.name = `box${Math.floor(Math.random()*10000000)}`;
+            console.log(newBox.name);
+            newBox.position.set(x + 2.05, y, z);
+            scene.add(newBox);
+            newBox = null;
+            return;
+          case 1:
+            newBox = createBox();
+            newBox.name = `box${Math.floor(Math.random()*10000000)}`;
+            newBox.position.set(x - 2.05, y, z);
+            scene.add(newBox);
+            newBox = null;
+            return;
+          case 2:
+            newBox = createBox();
+            newBox.name = `box${Math.floor(Math.random()*10000000)}`;
+            newBox.position.set(x, y + 2.05, z);
+            scene.add(newBox);
+            newBox = null;
+            return;
+          case 3:
+            return;
+          case 4:
+            newBox = createBox();
+            newBox.name = `box${Math.floor(Math.random()*10000000)}`;
+            newBox.position.set(x, y, z + 2.05);
+            scene.add(newBox);
+            newBox = null;
+            return;
+          case 5:
+            newBox = createBox();
+            newBox.name = `box${Math.floor(Math.random()*10000000)}`;
+            newBox.position.set(x, y, z - 2.05);
+            scene.add(newBox);
+            newBox = null;
+            return;
+        }
+      }
+    }
+  }
+}, false);
 
 
-const loop = () => {
-  boxes.forEach((box, i) => box.rotation.x += 0.0002*i);
+const render = () => {
+  requestAnimationFrame(render);
   renderer.render(scene, camera);
-  requestAnimationFrame(loop);
 };
 
-loop();
-window.addEventListener('resize', handleResize);
+render();
